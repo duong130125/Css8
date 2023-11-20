e9e63a6c88a2163de103eda66d6a9c3ffac9c3d3
duong130125 <147391418+duong130125@users.noreply.github.com>
2023-11-20 21:47:40 +0700

    Add files via upload

---
+++ BT4ss8.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+int main(){
+	int addValue, addIndex, n;
+	printf("Nhap vao so phan tu cua mang:");
+	scanf("%d",&n);
+	printf("Nhap vao gia tri can chen vao mang:");
+	scanf("%d",&addValue);
+	printf("Nhap vao chi so can chen vao mang:");
+	scanf("%d",&addIndex);
+	int oldNumbers[n];
+	printf("Nhap gia tri cac phan tu cua mang:\n");
+	for(int i=0;i<n;i++){
+		printf("oldNumbers[%d]=",i);
+		scanf("%d",&oldNumbers[i]);
+	}
+	if(addIndex<n){
+		int newNumbers[n+1];
+		for(int i=0; i<n+1; i++){
+			if(i<addIndex){
+				newNumbers[i] = oldNumbers[i];
+			}else if(i==addIndex){
+				newNumbers[i] = addValue;
+			}else{
+				newNumbers[i] = oldNumbers[i-1];
+			}		
+		}	
+		printf("Mang sau khi chen phan tu la:\n");
+		for(int i=0; i<n+1; i++){
+			printf("%d\t",newNumbers[i]);
+		}		
+	}else{
+		int newNumbers[addIndex+1];
+		for(int i=0; i<addIndex; i++){
+			if(i<n){
+				newNumbers[i] = oldNumbers[i];
+			}else{
+				newNumbers[i] = 0;
+			}
+		}			
+		newNumbers[addIndex] = addValue;
+		printf("Mang sau khi chen phan tu la:\n");
+		for(int i=0; i<=addIndex; i++){
+			printf("%d\t",newNumbers[i]);
+		}		
+	}
+	
+}
